14369118f196b398565161755337d7f9e8d2f7f4
RodolfoTorija <112714490+RodolfoTorija@users.noreply.github.com>
2023-04-19 23:37:26 -0600

    update para revision jueves

--- AnalizadorSemantico.py
+++ AnalizadorSemantico.py
@@ -4,7 +4,7 @@ import spacy
 nlp = spacy.load('es_core_news_sm')
 
 # Texto de ejemplo
-texto = 'El perro corre detrás del gato muy rapido'
+texto = 'El perro corre detras del gato'
 
 # Analizar el texto utilizando Spacy
 doc = nlp(texto)
@@ -22,6 +22,35 @@ for palabra in doc:
     elif palabra.pos_ == 'VERB':
         verbos.append(palabra.text)
 
+for palabra in doc:
+    print(palabra.text, palabra.dep_)
+
 # Imprimir los sustantivos y verbos encontrados
 print('Sustantivos encontrados:', sustantivos)
 print('Verbos encontrados:', verbos)
+
+# Definir función para buscar el sujeto
+def buscar_sujeto(verbo):
+    for palabra in verbo.lefts:
+        if palabra.dep_ == 'nsubj':
+            return palabra
+    return None
+
+# Definir función para buscar el objeto
+def buscar_objeto(verbo):
+    for palabra in verbo.rights:
+        if palabra.dep_ == 'obj':
+            return palabra
+    return None
+
+# Iterar sobre los verbos encontrados
+for verbo in verbos:
+    # Buscar el sujeto y el objeto para cada verbo
+    for palabra in doc:
+        if palabra.text == verbo:
+            sujeto = buscar_sujeto(palabra)
+            objeto = buscar_objeto(palabra)
+
+            # Imprimir la relación semántica entre el sujeto y el objeto
+            if sujeto and objeto:
+                print('Relación semántica:', sujeto.text, verbo, objeto.text)
